f727295ddb2c6e8e8336aca9ec7ab9e5414e3b27
Laura <laura.kauria@helsinki.fi>
2022-08-07 15:22:50 +0300

    added new.html (raw) to add new apartments. Updated requirements.txt, added database connection to app.py, extended gitigore with .vscode file

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask
-from flask import render_template 
-from flask import request
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///housing"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    result = db.session.execute("SELECT selling_price FROM sold_apartment")
+    apartments = result.fetchall()
+    return render_template("index.html", count=len(apartments), apartments=apartments) 
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
 
 @app.route("/result", methods=["POST"])
 def result():
     return render_template("result.html", name=request.form["username"])
+
